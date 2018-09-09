ac6797cd92f4cd1bbf3257252ec00c25e702f8af
fuyukang <308515050@qq.com>
2018-09-09 21:20:28 +0800

    删除不必要的包引入

--- app/src/main/java/me/yukang/litepaldemo/MainActivity.java
+++ app/src/main/java/me/yukang/litepaldemo/MainActivity.java
@@ -1,13 +1,12 @@
 package me.yukang.litepaldemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import org.litepal.LitePal;
-import org.litepal.crud.LitePalSupport;
 
 import java.util.List;
 
